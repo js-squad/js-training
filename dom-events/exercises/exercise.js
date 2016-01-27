ee9cb812b53d5634ebfec7c009ffded93ab424b8
Mauro Titimoli <mgtitimoli@gmail.com>
2016-01-27 10:27:38 -0300

    Added exercise

---
+++ dom-events/exercises/exercise.js
@@ -0,0 +1,5 @@
+// add the corresponding listeneres in order to
+// 1 - if a personality is selected it must be displayed on the right
+//     of the personality list
+// 2 - the browser must only redirect to the twitter handle if it is clicked
+//      on the personality selected (not on the list)
